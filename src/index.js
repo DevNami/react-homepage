f72acfc5cb917ae7d122639c0b176b404fd58893
nami4628 <nami4628@cafe24.com>
2018-05-07 13:15:33 +0900

    initialize

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'styles/main.scss';
+import Root from './Root';
+import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(Root)
+
+if (module.hot) {
+  module.hot.accept('./Root', () => { render(Root) })
+}
+
+registerServiceWorker();
